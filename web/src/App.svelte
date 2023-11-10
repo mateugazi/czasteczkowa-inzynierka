be9213c4342f2a35b12b16dd3d349a23f86ebd4b
mateugazi <mateugazi@protonmail.com>
2023-11-10 12:42:49 +0100

    web-first-attempt: example graph and file input flow to work on it later

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,9 +1,49 @@
 <script>
+  import * as d3 from 'd3';
+
+  let files;
+
+  export let data = d3.ticks(-2, 2, 200).map(Math.sin);
+  export let width = 640;
+  export let height = 400;
+  export let marginTop = 20;
+  export let marginRight = 20;
+  export let marginBottom = 30;
+  export let marginLeft = 40;
+
+  let gx;
+  let gy;
+
+  $: x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
+  $: y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
+  $: line = d3.line((d, i) => x(i), y);
+  $: d3.select(gy).call(d3.axisLeft(y));
+  $: d3.select(gx).call(d3.axisBottom(x));
+    function onMousemove(event) {
+		const [x, y] = d3.pointer(event);
+    data = data.slice(-200).concat(Math.atan2(x, y));
+  }
 </script>
 
-<main>
-  <div>yo</div>
-</main>
+<label for="avatar">Upload a file:</label>
+<input accept=".csv" bind:files id="avatar" name="avatar" type="file" />
 
-<style> 
-</style>
+{#if files}
+	<h2>Selected files:</h2>
+	{#each Array.from(files) as file}
+		<p>{file.name} ({file.size} bytes)</p>
+	{/each}
+{/if}
+
+<div on:mousemove={onMousemove}>
+  <svg width={width} height={height}>
+    <g bind:this={gx} transform="translate(0,{height - marginBottom})" />
+    <g bind:this={gy} transform="translate({marginLeft},0)" />
+    <path fill="none" stroke="currentColor" stroke-width="1.5" d={line(data)} />
+    <g fill="white" stroke="currentColor" stroke-width="1.5">
+      {#each data as d, i}
+        <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+      {/each}
+    </g>
+  </svg>
+</div>
