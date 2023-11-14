a7aa6681047f08ff272786fd439be6b4746ced4a
mateugazi <mateugazi@protonmail.com>
2023-11-15 00:15:22 +0100

    web-first-attempt: Added simple landing page and ability to upload csv

---
+++ web/src/components/FileReceiver.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { createEventDispatcher } from "svelte";
+  let files;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<input accept=".csv" bind:files type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+
+{#if files}
+  <button class="btn btn-primary btn-lg" on:click={() => dispatch('triggercalculations')}>Run predictions</button>
+{/if}
+
+<style>
+  .btn {
+    margin-top: 30px;
+  }
+</style>
+
---
+++ web/src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<div class='navbar-container'>
+  <div class="navbar bg-primary text-primary-content">
+    <button class="btn btn-ghost text-xl">cząsteczki</button>
+  </div>
+</div>
+
+<style>
+  .navbar-container {
+    display: flex;
+  }
+
+  .navbar {
+    border-radius: 10px;
+    margin: 10px;
+  }
+</style>
