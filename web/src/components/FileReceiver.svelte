a34ad8cb6137511a9ed35960751be8576c95b1ce
mateugazi <mateugazi@protonmail.com>
2023-11-25 12:48:12 +0100

    EX: Created server and added endpoint that is able to read csv and return prediction and web that can create request with csv

--- web/src/components/FileReceiver.svelte
+++ web/src/components/FileReceiver.svelte
@@ -3,12 +3,54 @@
   let files;
 
 	const dispatch = createEventDispatcher();
+	let foo = 'baz'
+	let bar = 'qux'
+  let result = null;
+
+  async function doPost () {
+		// const res = await fetch('http://localhost:3000/get-predictions', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify({'pozdro': '600'}),
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+		// })
+		
+		// const json = await res.json();
+	  // result = JSON.stringify(json);
+    // console.log(result);
+
+    const data = new FormData();
+    data.append('file', files[0]);
+    const res = await fetch('http://localhost:3000/upload-csv', {
+			method: 'POST',
+			body: data,
+		})
+		
+		const json = await res.json();
+	  result = JSON.stringify(json);
+    console.log(result);
+
+    // const data = new FormData();
+    // data.append('file', files[0]);
+    // const xhr = new XMLHttpRequest();
+
+    // xhr.addEventListener("readystatechange", function () {
+    //   if (this.readyState === this.DONE) {
+    //     console.log(this.responseText);
+    //   }
+    // });
+
+    // xhr.open("POST", "http://localhost:3000/upload-csv");
+
+    // xhr.send(data);
+  }
 </script>
 
 <input accept=".csv" bind:files type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs" />
 
 {#if files}
-  <button class="btn btn-primary btn-lg" on:click={() => dispatch('triggercalculations')}>Run predictions</button>
+  <button class="btn btn-primary btn-lg" on:click={() => doPost()}>Run predictions</button>
 {/if}
 
 <style>
