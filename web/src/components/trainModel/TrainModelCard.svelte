e3b8fc33eef339ead13cf47a023e8279a2baf726
mateugazi <mateugazi@protonmail.com>
2024-01-07 11:49:46 +0100

    train-model: card refactor

---
+++ web/src/components/trainModel/TrainModelCard.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { getModels } from "../../helpers/getModels";
+	import { store } from "../../store/store";
+	import TrainModelModal from "./TrainModelModal.svelte";
+	export let title;
+	export let description;
+	export let iconPath;
+
+	let models;
+
+	const unsubscribe = store.subscribe((state) => {
+		models = state.models;
+	});
+
+	const showModal = async () => {
+		document.getElementById("train-model-modal").showModal();
+	};
+</script>
+
+<div class="card w-96 bg-base-100 shadow-xl">
+	<figure>
+		<img width="182px" src={iconPath} alt="Icon" />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{description}</p>
+		<div class="card-actions justify-end">
+			<button class="btn" on:click={showModal}>Select</button>
+			<TrainModelModal />
+		</div>
+	</div>
+</div>
+
+<style>
+	p {
+		text-align: start;
+	}
+
+	figure {
+		padding-top: 5rem;
+		padding-bottom: 5rem;
+	}
+
+	.card {
+		width: 50%;
+	}
+</style>
---
+++ web/src/components/trainModel/TrainModelModal.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<dialog id="train-model-modal" class="modal">
+	<div class="modal-box w-11/12 max-w-5xl">
+		<h2 class="font-bold text-3xl">Create model</h2>
+		<div class="card w-96 bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Form title</h2>
+				<p>Form description</p>
+				<div class="card-actions justify-end">
+					<button class="btn btn-primary" on:click={() => console.log("to do")}
+						>Select</button
+					>
+				</div>
+			</div>
+		</div>
+		<div class="modal-action">
+			<form method="dialog">
+				<button class="btn">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
