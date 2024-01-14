61eb58ae79196b3f8eb5d61857f37ea1ec526ce8
TwistedTaleTeller <wojciechmajewski330@gmail.com>
2024-01-14 21:36:58 +0100

    Classification 0.1 to 0.4 results
    with partial 0.5

--- experiments/Standardized Pipeline/full_hyperparameter_search.py
+++ experiments/Standardized Pipeline/full_hyperparameter_search.py
@@ -16,7 +16,7 @@ if name == "Wojtek":
     calculate_descriptors = False
     calculate_fingerprints = True
 
-for regression in [True, False]:
+for regression in [False]:
     if regression:
         param_grid_dt={
             'max_depth': [None, 10, 20], 'min_samples_split': [2, 5, 10], 'min_samples_leaf': [1, 2, 4],
